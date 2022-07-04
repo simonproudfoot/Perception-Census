5f92f164e5faf17000740ff5c9d36e8b217c3cb4
simonproudfoot <simonproudfoot1980@gmail.com>
2022-07-04 19:00:27 +0100

    mobile text slider

--- js/app.js
+++ js/app.js
@@ -15,11 +15,9 @@ var app = new Vue({
    
         let step1 = gsap.timeline({
             scrollTrigger: {
-        
              // toggleActions: 'play pause resume pause',
                 trigger: '#container',
                 start: 'top top',
-                
                 end: '+=' + height * 3,
                 scrub: 2,
                 pin: true,
@@ -34,11 +32,10 @@ var app = new Vue({
                 },
             }
         })
-        gsap.set(wrapper, {top: '85%'})
-        step1.to(wrapper, { y: -height - 100, duration: 3 })
-        step1.fromTo(wrapper, { opacity: 0.3, borderRadius: "100%", height: width }, { y: '-85%', duration: 3, borderRadius: "0%", opacity: 1, height: '100vh' })
+        gsap.set(wrapper, {top: '70%'})
+        step1.to(wrapper, { y: -height/2 - 100, duration: 3 })
+        step1.fromTo(wrapper, { opacity: 0.3, borderRadius: "100%", height: width }, { y: '-70%', duration: 3, borderRadius: "0%", opacity: 1, height: '100vh' })
       
-       
         step1.to(".hero", {
             y: -height / 2, opacity: 0, ease: "none", duration: 4,
         }, '-=6')
@@ -46,9 +43,7 @@ var app = new Vue({
         step1.to(wrapper, { duration: 10 })
         step1.from("#play-button", {bottom: -20, opacity: 0,  duration: 2},'-=10')
 
-
-        
-        gsap.set('.front-page-item', { opacity: 0, top: 200 })
+     
         let step2 = gsap.timeline({
             defaults: {
                 ease: "none"
@@ -64,9 +59,11 @@ var app = new Vue({
                
             }
         })
+        gsap.set('.front-page-item-new', { opacity: 0, yPercent: 20 })
+        step2.to('.front-page-item-new', { yPercent: 0, stagger: 2, opacity: 1, delay: 0, duration: 1 })
+        step2.to('.front-page-item-new', { yPercent: -20, stagger: 2, opacity: 0, duration: 1, }, '-=1')
 
-        step2.to('.front-page-item', { top: 0, stagger: 2, opacity: 1, delay: 0, duration: 1 })
-        step2.to('.front-page-item', { top: -100, stagger: 2, opacity: 0, duration: 1, }, '-=1')
+        
         let progressTl = gsap.timeline({
             scrollTrigger: {
                // toggleActions: 'restart pause reverse pause',
